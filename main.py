027eed1d60a48333799fba5dcd41ba4a94bb1b10
stzucker <35130875+stzucker@users.noreply.github.com>
2020-07-07 10:18:01 +0300

    Update main.py
    Перенёс по памяти из своего проекта на ноутбуке классы

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hi the world')
+class SMO:
+  pass
+
+class ManyChannel_SMO_unlimited_queue(SMO):
+  pass
+
+class ManyChannel_SMO_limited_queue(SMO):
+  pass
+
+class ManyChannel_SMO_with_denial(SMO):
+  pass
+
+class OneChannel_SMO_unlimited_queue(SMO):
+  pass
+
+class OneChannel_SMO_limited_queue(SMO):
+  pass
+
+class OneChannel_SMO_with_denial(SMO):
+  pass
